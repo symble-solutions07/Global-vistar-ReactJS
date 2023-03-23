ae777116a43b9861d2f75190ae2dbe095edd00ee
Pranav-1998 <pranavbankar32@gmail.com>
2023-03-23 23:05:15 +0530

    Co-authored-by: Atharva Kurumbhatte <Heisenberg1607@users.noreply.github.com>

--- src/Header.jsx
+++ src/Header.jsx
@@ -95,7 +95,7 @@ function Header() {
               <ul className="navbar-list">
                 <li>
                   <a href="#home" className="navbar-link" data-nav-link>
-                    Demo
+                    Home
                   </a>
                 </li>
                 <li>
