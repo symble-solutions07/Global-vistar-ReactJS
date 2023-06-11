58c8d321a46d2503636fe97496354855c87328b8
Heisenberg1607 <76523428+Heisenberg1607@users.noreply.github.com>
2023-06-11 15:48:30 +0530

    changes infocards and calendly

--- src/Components/BookADemoCall.jsx
+++ src/Components/BookADemoCall.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "../style.css";
 import "../BookADemo.css"
+import { PopupButton } from "react-calendly";
+
 
 function BookADemoCall() {
   return (
     <div>
       <div className="book-a-demo-section">
         <h1>Not sure? Book a demo call.</h1>
-        <button className="card-footer-actions-btn call-btn" >Schedule a tour</button>
+        <div>
+          <PopupButton
+            className="call-btn"
+            url="https://calendly.com/d/z83-fx5-wdf/30min"
+            /*
+             * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
+             * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
+             */
+            rootElement={document.getElementById("root")}
+            text="Schedule A Call With Us"
+          />
+        </div>
       </div>
     </div>
   );
