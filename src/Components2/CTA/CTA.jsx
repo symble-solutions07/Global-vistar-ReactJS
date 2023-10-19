4ff9f29b1bca380db25c14097823f8de12331ffa
Gaikwad Amey Kisan <amey124g@gmail.com>
2023-10-19 22:52:17 +0530

    minor fixes

--- src/Components2/CTA/CTA.jsx
+++ src/Components2/CTA/CTA.jsx
@@ -1,6 +1,7 @@
 import './cta.css'
 import GVlogo from "./Images/global-vistar-logo.png"
 import RegisterImg from "./Images/register-img.png"
+import { Link } from "react-router-dom";
 
 export default function CTA() {
   return (
@@ -8,7 +9,8 @@ export default function CTA() {
         <div className="cta-child">
             <h1 className='cta-title'>Ready to get started?</h1>
             <p className="cta-p">Badhaye Aapke Business Ki Raftaar by Signing Up Today!</p>
-            <button className="cta-btn">Register Now</button>
+            <Link to="/register"><button className="cta-btn">Register Now</button></Link>
+            
             <img className='cta-child-img' src={RegisterImg} alt="" />
         </div>
         <div className="contact-info">
