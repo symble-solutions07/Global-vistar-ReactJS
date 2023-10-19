02a32dfccaff451fac5c6e119100fcfefedaca25
Gaikwad Amey Kisan <amey124g@gmail.com>
2023-10-19 10:51:27 +0530

    Changes to featured products card

--- src/Components2/Featured Products/FeaturedProduct.jsx
+++ src/Components2/Featured Products/FeaturedProduct.jsx
@@ -26,9 +26,9 @@ export default function FeaturedProduct() {
           <div className="featured-products-card-description">
             <p className="featured-products-card-verified">Global Vistar Verified</p>
             <h1 className="featured-products-card-title">Desi Rass</h1>
-            <p className="featured-products-card-price">Price: ₹7200</p>
-            <p className="featured-products-card-price">MOQ: 1 Box - 120 nos</p>
-            <p className="featured-products-card-margin">Margin: 20%-30%</p>
+            <p className="featured-products-card-price">Price: ₹---</p>
+            <p className="featured-products-card-price">MOQ: 100 boxes</p>
+            <p className="featured-products-card-margin">Margin: 35%-45%</p>
             <button className="featured-products-card-btn">Send Enquiry</button>
           </div>
         </div>
@@ -37,8 +37,8 @@ export default function FeaturedProduct() {
           <div className="featured-products-card-description">
             <p className="featured-products-card-verified">Global Vistar Verified</p>
             <h1 className="featured-products-card-title">Shivratna Jaggery</h1>
-            <p className="featured-products-card-price">Price: ₹7200</p>
-            <p className="featured-products-card-price">MOQ: 1 Box - 120 nos</p>
+            <p className="featured-products-card-price">Price: ₹48000</p>
+            <p className="featured-products-card-price">MOQ: 1000kg</p>
             <p className="featured-products-card-margin">Margin: 20%-30%</p>
             <button className="featured-products-card-btn">Send Enquiry</button>
           </div>
@@ -48,7 +48,7 @@ export default function FeaturedProduct() {
           <div className="featured-products-card-description">
             <p className="featured-products-card-verified">Global Vistar Verified</p>
             <h1 className="featured-products-card-title">Tribe Cook</h1>
-            <p className="featured-products-card-price">Price: ₹7200</p>
+            <p className="featured-products-card-price">Price: ₹---</p>
             <p className="featured-products-card-price">MOQ: 1 Box - 120 nos</p>
             <p className="featured-products-card-margin">Margin: 20%-30%</p>
           <button className="featured-products-card-btn">Send Enquiry</button>
