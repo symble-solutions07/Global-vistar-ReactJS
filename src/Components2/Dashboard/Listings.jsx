8d6e06574e777dfc0a89d40db64e5e6e43920b2b
Gaikwad Amey Kisan <amey124g@gmail.com>
2023-12-29 11:06:44 +0530

    dashboard completion

---
+++ src/Components2/Dashboard/Listings.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import FeaturedProductCard from '../ProductsPage/ProductCard';
+import "./listings.css"
+
+function Listings() {
+    const [products, setProducts] = useState([]);
+
+     useEffect(() => {
+       const fetchProducts = async () => {
+         try {
+           const response = await fetch(
+            //  "http://localhost:3001/user/allProducts",
+             "https://globalvistarbackend-production.up.railway.app/user/allProducts",
+             {
+               method: "GET",
+               headers: {
+                 authorization: "Bearer " + localStorage.getItem("token"),
+               },
+             }
+           ); // Replace with your backend endpoint
+           const data = await response.json();
+           console.log(data.products[0].image1);
+           setProducts(data.products);
+         } catch (error) {
+           console.error("Error fetching products: ", error);
+         }
+       };
+
+       fetchProducts();
+     }, []);
+
+  return (
+    <div className='listings'>
+      <div className="productsListingWrapper">
+        {products.map((product, index) => (
+          <FeaturedProductCard
+            keyy={product._id}
+            imageLink={product.image1}
+            title={product.title}
+            price={product.price2}
+            moq={product.minOrderQuantity}
+            margin={product.expectedMargin}
+            // handleSendEnquiry={handleSendEnquiry}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+export default Listings
