c21af6e34efdd838d52743451914073e31f966ee
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-02-23 10:48:48 +0530

    backend link

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:3001"
-// export const BACKEND_URL =
-//   "https://globalvistarbackend-production.up.railway.app";
+// export const BACKEND_URL = "http://localhost:3001"
+export const BACKEND_URL =
+  "https://globalvistarbackend-production.up.railway.app";
