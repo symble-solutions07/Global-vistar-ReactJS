06d806994307ac5db560322e05ed20faa943a587
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2024-04-17 00:11:30 +0530

    .

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:3001"
-export const BACKEND_URL = "https://globalvistarbackend.onrender.com/";
+export const BACKEND_URL = "https://globalvistarbackend.onrender.com";
